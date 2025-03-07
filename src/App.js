0721d1d21c657bdb5b077fa9e30049a58d27936a
Esrakgun <akgundogduesr@gmail.com>
2025-03-08 00:54:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Pagecontainer from "./containers/Pagecontainer";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Details from "./pages/Details";
+import Cart from "./pages/Cart";
+import Search from "./pages/Search";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Pagecontainer>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/products/:id" element={<Details/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/products/:search" element={<Search/>} />
+        </Routes>
+      </BrowserRouter>
+      </Pagecontainer>
     </div>
   );
 }
